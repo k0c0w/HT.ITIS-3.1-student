93cebc7583d166c072edf8bc69e9ef3709674bf4
Artur Yakupov <art.kazan@mail.ru>
2023-10-15 12:17:14 +0300

    Implemented Controllers/ProductManagementController.cs

--- Dotnet.Homeworks.MainProject/Controllers/ProductManagementController.cs
+++ Dotnet.Homeworks.MainProject/Controllers/ProductManagementController.cs
@@ -1,3 +1,9 @@
+using Dotnet.Homeworks.Domain.Exceptions;
+using Dotnet.Homeworks.Features.Products.Commands.DeleteProduct;
+using Dotnet.Homeworks.Features.Products.Commands.InsertProduct;
+using Dotnet.Homeworks.Features.Products.Commands.UpdateProduct;
+using Dotnet.Homeworks.Features.Products.Queries.GetProducts;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dotnet.Homeworks.MainProject.Controllers;
@@ -5,27 +11,67 @@ namespace Dotnet.Homeworks.MainProject.Controllers;
 [ApiController]
 public class ProductManagementController : ControllerBase
 {
-    [HttpGet("products")]
-    public Task<IActionResult> GetProducts(CancellationToken cancellationToken)
+    private static readonly GetProductsQuery GetProductsQuery = new GetProductsQuery();
+
+    private readonly ISender _cqrsSender;
+
+    public ProductManagementController(ISender cqrs)
     {
-        throw new NotImplementedException();
+        _cqrsSender = cqrs;
+    }
+
+    [HttpGet("products")]
+    public async Task<IActionResult> GetProductsAsync(CancellationToken cancellationToken)
+    {
+        var products = await _cqrsSender.Send(GetProductsQuery, cancellationToken).ConfigureAwait(false);
+
+        return new JsonResult(products);
     }
 
     [HttpPost("product")]
-    public Task<IActionResult> InsertProduct(string name, CancellationToken cancellationToken)
+    public async Task<IActionResult> InsertProduct([FromBody] string name, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(name))
+            return BadRequest(new { error = "Name can not be empty" });
+
+        var insertedProductId = await _cqrsSender.Send(new InsertProductCommand(name), cancellationToken).ConfigureAwait(false);
+
+        return Created(Url.RouteUrl(GetProductsAsync), insertedProductId);
     }
 
     [HttpDelete("product")]
-    public Task<IActionResult> DeleteProduct(Guid guid, CancellationToken cancellationToken)
+    public async Task<IActionResult> DeleteProduct(Guid guid, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        if (guid == default)
+            return BadRequest(new { error = "Wrong guid" });
+
+        try
+        {
+            await _cqrsSender.Send(new DeleteProductByGuidCommand(guid), cancellationToken).ConfigureAwait(false);
+        }
+        catch (EntityNotFoundException)
+        {
+            return NotFound(guid);
+        }
+
+        return NoContent();
     }
 
     [HttpPut("product")]
-    public Task<IActionResult> UpdateProduct(Guid guid, string name, CancellationToken cancellationToken)
+    public async Task<IActionResult> UpdateProduct(Guid guid, string name, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        if (guid == default)
+            return BadRequest(new { error = "Wrong guid" });
+
+        try
+        {
+            await _cqrsSender.Send(new UpdateProductCommand(guid, name), cancellationToken).ConfigureAwait(false);
+        }
+        catch (EntityNotFoundException)
+        {
+            return NotFound(guid);
+        }
+
+        return NoContent();
     }
 }
