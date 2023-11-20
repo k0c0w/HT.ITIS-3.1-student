2e5e59a584b386fc4274a62a69acddd0dd8f9fd9
Artur Yakupov <art.kazan@mail.ru>
2023-11-20 09:42:48 +0300

    Added DeleteUser/DeleteUserCommandHandler.cs

--- Dotnet.Homeworks.Features/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
+++ Dotnet.Homeworks.Features/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -1,5 +1,38 @@
-﻿namespace Dotnet.Homeworks.Features.Users.Commands.DeleteUser;
+﻿using Dotnet.Homeworks.Domain.Abstractions.Repositories;
+using Dotnet.Homeworks.Infrastructure.Cqrs.Commands;
+using Dotnet.Homeworks.Infrastructure.UnitOfWork;
+using Dotnet.Homeworks.Shared.Dto;
 
-public class DeleteUserCommandHandler //TODO: Inherit certain interface 
+namespace Dotnet.Homeworks.Features.Users.Commands.DeleteUser;
+
+public class DeleteUserCommandHandler : ICommandHandler<DeleteUserCommand>
 {
+    private readonly IUserRepository _userRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+
+    public DeleteUserCommandHandler(IUserRepository userRepository, IUnitOfWork unitOfWork)
+    {
+        _userRepository = userRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Result> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            await _userRepository.DeleteUserByGuidAsync(request.Guid, cancellationToken);
+
+            if (cancellationToken.IsCancellationRequested)
+                return new Result(false);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return new Result(true);
+        }
+        catch
+        {
+            return new Result(false);
+        }
+    }
 }
