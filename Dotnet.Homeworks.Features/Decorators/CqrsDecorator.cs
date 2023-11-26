6134b8d04e5a977e9d2c2599f2ff9733d140f16b
Artur Yakupov <art.kazan@mail.ru>
2023-11-26 15:38:45 +0300

    Inverted decorators since it was unable to resolve types

--- Dotnet.Homeworks.Features/Decorators/CqrsDecorator.cs
+++ Dotnet.Homeworks.Features/Decorators/CqrsDecorator.cs
@@ -1,23 +1,16 @@
-﻿using Dotnet.Homeworks.Infrastructure.Validation.Decorators;
-using Dotnet.Homeworks.Infrastructure.Validation.PermissionChecker;
+﻿using Dotnet.Homeworks.Infrastructure.Validation.PermissionChecker;
+using Dotnet.Homeworks.Infrastructure.Validation.RequestTypes;
 using Dotnet.Homeworks.Mediator;
 using FluentValidation;
 
 namespace Dotnet.Homeworks.Features.Decorators;
 
 
-public class CqrsDecorator<TRequest, TResponse> : ValidationPermissionCheckDecorator<TRequest, TResponse> where TRequest : IRequest<TResponse>
+public class CqrsDecorator<TRequest, TResponse> : UserPermissionCheckValidationDecorator<TRequest, TResponse> where TRequest : IRequest<TResponse>
 {
 
-    public CqrsDecorator(IEnumerable<IValidator<TRequest>> validators, IPermissionCheck<TRequest>? permissionCheck)
+    public CqrsDecorator(IEnumerable<IValidator<TRequest>> validators, IPermissionCheck<IClientRequest> permissionCheck)
         : base(validators, permissionCheck)
     {
-    }
-
-    public override async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken)
-    {
-        var pipelineResult = await Handle(request, cancellationToken);
-
-        return pipelineResult;
     }
 }
---
+++ Dotnet.Homeworks.Features/Decorators/UserPermissionCheckValidationDecorator.cs
@@ -0,0 +1,30 @@
+﻿using Dotnet.Homeworks.Infrastructure.Validation.PermissionChecker;
+using Dotnet.Homeworks.Infrastructure.Validation.RequestTypes;
+using Dotnet.Homeworks.Mediator;
+using Dotnet.Homeworks.Shared.Dto;
+using FluentValidation;
+
+namespace Dotnet.Homeworks.Features.Decorators;
+
+public class UserPermissionCheckValidationDecorator<TRequest, TResponse> : ValidationkDecorator<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    private readonly IPermissionCheck<IClientRequest> _permissionCheck;
+
+    public UserPermissionCheckValidationDecorator(IEnumerable<IValidator<TRequest>> validators, IPermissionCheck<IClientRequest> permissionCheck) : base(validators)
+    {
+        _permissionCheck = permissionCheck;
+    }
+
+    public override async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken)
+    {
+        if (request is not IClientRequest userRequest)
+            return await base.Handle(request, cancellationToken);
+
+        var permissionCheckResult = await _permissionCheck.CheckPermissionAsync(userRequest);
+        var failures = permissionCheckResult.Where(x => x.IsFailure).ToArray();
+        if (failures.Length == 0)
+            return await base.Handle(request, cancellationToken);
+
+        return new Result<TResponse>(default, false, string.Join(' ', failures.Select(x => x.Error))) as dynamic;
+    }
+}
