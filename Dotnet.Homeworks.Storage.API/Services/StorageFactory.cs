9c3094185292ae4762ad55468cf89e9ea9c1c995
Artur Yakupov <art.kazan@mail.ru>
2023-12-19 11:39:29 +0300

    implemented ImageStorage.cs and StorageFactory

--- Dotnet.Homeworks.Storage.API/Services/StorageFactory.cs
+++ Dotnet.Homeworks.Storage.API/Services/StorageFactory.cs
@@ -1,13 +1,21 @@
 ﻿using Dotnet.Homeworks.Storage.API.Dto.Internal;
+using Minio;
 
 namespace Dotnet.Homeworks.Storage.API.Services;
 
 public class StorageFactory : IStorageFactory
 {
+    private readonly IMinioClient _client;
+
+    public StorageFactory(IMinioClient client)
+    {
+        _client = client;
+    }
+
     public Task<IStorage<Image>> CreateImageStorageWithinBucketAsync(string bucketName)
     {
-        // TODO: implement creation of IImageStorage with the given bucketName
-        // e.g. each storage should work only within its bucket (but still may copy items to another bucket)
-        throw new NotImplementedException();
+        IStorage<Image> storage = new ImageStorage(_client, bucketName);
+
+        return Task.FromResult(storage);
     }
 }
